33d6849c9ed92df4808232562c0cee35b823ed72
pelmen964 <ivanklimov41234@gmail.com>
2023-02-28 20:45:51 +0700

    model ai deploing

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from keras.layers import Dense, Flatten
+from keras.datasets import mnist
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+x_train = x_train / 255
+x_test =  x_test / 255
+
+y_train_cat = keras.utils.to_categorical(y_train, 10)
+y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+model = keras. Sequential([
+    Flatten(input_shape=(28,28,1)),
+    Dense(units=128, activation='relu'),
+    Dense(units=10, activation='softmax')
+        ])
+
+model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2)
+print("\n")
+
+model.evaluate(x_test, y_test_cat)
+print("\n")
+
+# Тест конкретной цифры хуй
+n = 1 # id в выборке mnist
+x = np.expand_dims(x_test[n], axis=0)
+res = model.predict(x)
+print( res )
+print( np.argmax(res) )
+
+plt.imshow(x_test[n], cmap=plt.cm.binary)
+plt.show()
+
+# # Распознавание всей тестовой выборки
+# pred = model.predict(x_test)
+# pred = np.argmax(pred, axis=1)
+#
+# print(pred.shape)
+#
+# print(pred[:20])
+# print(y_test[:20])
+#
+# # Выделение неверных вариантов
+# mask = pred == y_test
+# print(mask[:10])
+#
+# x_false = x_test[~mask]
+# y_false = x_test[~mask]
+#
+# print(x_false.shape)
+#
+# # Вывод первых 25 неверных результатов
+# plt.figure(figsize=(10,5))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.imshow(x_false[i], cmap=plt.cm.binary)
+#
+# plt.show()
