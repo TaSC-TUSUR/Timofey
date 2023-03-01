f9e5dbce03f4542fa5dd280a593cde3980d09b12
pelmen964 <ivanklimov41234@gmail.com>
2023-03-01 18:49:08 +0700

    Adding experimental converter

--- converter.py
+++ converter.py
@@ -6,13 +6,32 @@
 
 import numpy as np
 from PIL import Image, ImageOps
-im = Image.open('images/raw/test_image5.jpg')
 
-im = im.convert('P')
-arr = np.array(im)
-arr[arr>135] = 255
-arr[arr<=135] = 0
-arr = np.array(Image.fromarray(arr))
-im = ImageOps.invert(Image.fromarray(arr)).resize((28, 28))
+def convert_im(path):
+    img = Image.open(path)
+    arr = np.array(img)
 
-im.save('images/assets/test.jpg')
+    # Мы блять надеемся на то, фон белый или серый, а у него соотношение цветов приерно одно и тоже,
+    # Например r,g,b = 130 126 123 - это сероватый цвет максимальная разница 130-123 = 7, что немного
+    # А у цвета r,g,b = 30 50 223 - это один из синих максимальная разница = 223-30 = 193, что дохуя
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            r, g, b = map(int, arr[i, j])
+            if abs(r - g) <= 30 \
+                and abs(r - b) <= 30\
+                and abs(g - b) <= 30\
+                and sum(arr[i, j]) > 100: # Проверим ещё черный цвет
+
+                    arr[i, j] = [255, 255, 255]
+            else:
+                arr[i, j] = [0, 0, 0]
+
+    arr = np.array(Image.fromarray(arr))
+    img = ImageOps.invert(Image.fromarray(arr)).resize((28, 28)) # Делаем его 28*28 как в базе данных
+    img = img.convert('L') # Делаем чб
+
+    img.save('images/assets/test.jpg')
+
+    # return img_array # Надо будет что-то придумать
+
+convert_im("images/raw/test_image1.jpg")
